b8acde2fb3ae1ce4c949446ff947fcabadb070b9
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-22 09:28:59 +0900

    Created exercise schema

---
+++ server/Models/User.models.js
@@ -0,0 +1,22 @@
+// Requirements
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
