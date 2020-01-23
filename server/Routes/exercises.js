2173d8715b78245efb9d774c8647dc03ce1eab13
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-23 21:56:25 +0900

    Adding delete route

--- server/Routes/exercises.js
+++ server/Routes/exercises.js
@@ -31,5 +31,10 @@ router.route("/:id").get((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted."))
+    .catch(err => res.status(400).json("Error: " + err));
+});
 
 module.exports = router;
