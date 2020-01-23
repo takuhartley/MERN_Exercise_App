291ba2d436287cc5d29f93baa9ba5621a2c02bb9
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-23 21:54:14 +0900

    Adding id route

---
+++ server/Routes/exercises.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+let Exercise = require("../Models/Exercise.models");
+
+router.route("/").get((req, res) => {
+  Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json("Error dwag: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = Date.parse(req.body.date);
+
+  const newExersice = new Exercise({
+    username,
+    description,
+    duration,
+    date
+  });
+  newExersice
+    .save()
+    .then(() => res.json("Exercise added dawg!"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+module.exports = router;
---
+++ server/Routes/users.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+let User = require("../Models/User.models");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json("Error dawg: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const newUser = new User({ username });
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch(err => res.status(400).json("Error dawwg: " + err));
+});
+
+module.exports = router;
